0b0a55c0517c826194fd63a8797b0786275b78f0
asdf0003 <58287113+asdf0003@users.noreply.github.com>
2020-01-09 01:12:32 +0800

    Update and rename src/test/java/com/yanceyzhang/chatbot/demo/TestConfig.java to  src/ test/ java/ com/ yanceyzhang/ chatbot/演示/TestConfig.java

---
+++ src/ test/ java/ com/ yanceyzhang/ chatbot/演示/TestConfig.java
@@ -0,0 +1,9 @@
+package com.yanceyzhang.chatbot.demo;
+
+/**
+ */
+public class TestConfig {
+	
+    //TODO use YOUR chatbot webhook
+    public static final String CHATBOT_WEBHOOK = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5085edb2-358d-48f8-9106-e9da2195097b;
+}
